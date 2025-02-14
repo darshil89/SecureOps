6c7bbd16cb35e9d41a31e0f105720a6d545f6a8d
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-02-14 16:33:20 +0530

    added login to landing page

--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,6 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Role } from "@prisma/client";
 
 const HeroSection: React.FC = () => {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    const handleSignIn = async () => {
+        await signIn("google");
+    };
+
+
+
+    useEffect(() => {
+        console.log(session?.user.role);
+
+        if (session) {
+            if (session.user.role === Role.AGENCY) {
+                router.push("/agency/dashboard");
+            }
+            if (session.user.role === Role.GUARD) {
+                router.push("/guard/dashboard");
+            }
+            if (session.user.role === Role.POLICE) {
+                router.push("/police/dashboard");
+            }
+            if (session.user.role === Role.USER) {
+                router.push("/user/dashboard");
+            }
+        }
+    }, [session]);
     return (
         <section className="relative max-w-screen-xl mx-auto py-4 px-4 md:px-8">
             <div className="absolute top-0 left-0 w-full h-full bg-white opacity-40"></div>
@@ -12,20 +43,20 @@ const HeroSection: React.FC = () => {
                     <p className="text-gray-500 leading-relaxed mt-3">
                         Nam erat risus, sodales sit amet lobortis ut, finibus eget metus. Cras aliquam ante ut tortor posuere feugiat. Duis sodales nisi id porta lacinia.
                     </p>
-                    <a
+                    <button
                         className="mt-5 px-4 py-2 text-indigo-600 font-medium bg-indigo-50 rounded-full inline-flex items-center"
-                        href="#">
+                        onClick={handleSignIn}>
                         Try it out
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-1 duration-150" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 7l5 5m0 0l-5 5m5-5H6" />
                         </svg>
-                    </a>
+                    </button>
                 </div>
                 <div className="flex-1 mt-5 mx-auto sm:w-9/12 lg:mt-0 lg:w-auto">
-                    <img 
-                        src="https://i.postimg.cc/kgd4WhyS/container.png" 
-                        alt="Hero Image" 
-                        className="w-full" 
+                    <img
+                        src="https://i.postimg.cc/kgd4WhyS/container.png"
+                        alt="Hero Image"
+                        className="w-full"
                     />
                 </div>
             </div>
